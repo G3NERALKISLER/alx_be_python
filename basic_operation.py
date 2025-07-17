51985b09cab3653da604ad08279b2d7b818a1f26
James kamau <jnk13576@gmail.com>
2025-07-17 02:36:01 -0900

    added a file to a folder

---
+++ basic_operation.py
@@ -0,0 +1,12 @@
+number1=10
+number2=5
+
+result = (number1+number1)
+
+print("The addition is:",result)
+answer = (number1-number2)
+
+print("The difference is :" ,answer)
+answers = (number1*number1)
+
+print("The multiplication is:",answers)
