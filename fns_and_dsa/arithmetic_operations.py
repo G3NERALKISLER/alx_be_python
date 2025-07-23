c5fcb5e4f2e9f50bfbb6af2be182f77fb25554b4
James kamau <jnk13576@gmail.com>
2025-07-23 01:04:23 -0900

    work this time

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,8 @@ def perform_operation(num1, num2, operation):
     elif operation == 'divide':
         if num2 == 0:
             return "cannot divide by zero"
+        elif num2 != 0:
+            return num1 /num2
         else:
             return float('inf')  # Division by zero
     else:
