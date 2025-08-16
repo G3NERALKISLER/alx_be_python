43829f90cf47f3fe69b2589edc3356bb0d1b1378
James kamau <jnk13576@gmail.com>
2025-08-16 16:55:40 +0300

    hope is now good

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,17 +1,16 @@
 # book_class.py
 
 class Book:
-    def __init__(self, title: str, author: str, year: int):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
 
     def __del__(self):
-        # Important: destructor only prints when object is deleted
         print(f"Deleting {self.title}")
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
